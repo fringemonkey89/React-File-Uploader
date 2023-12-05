6668c7e13b32fdb23fb4afa0b9110b2a4ebdbdca
fringemonkey89 <markomajeticjr@gmail.com>
2023-12-05 14:02:53 -0500

    added file upload file

---
+++ client/src/App.js
@@ -0,0 +1,9 @@
+import './App.css';
+
+const App = () => <div className='container mt-4'>
+  <h4 className='display-4 text-center mb-4'>
+    React File Upload
+  </h4>
+</div> 
+
+export default App;
